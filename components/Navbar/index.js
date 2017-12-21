2c8020d2fc62c335b18d025b7c9c70697934b35d
noney1412 <nonpanpila@gmail.com>
2017-12-21 22:42:34 +0700

    navbar

---
+++ components/Navbar/index.js
@@ -0,0 +1,10 @@
+import { Navbar } from "./styled";
+
+export default () => (
+	<Navbar>
+		<a href="">Introduction</a>
+		<a href="">Skill</a>
+		<a href="">Achievement</a>
+		<a href="">Project</a>
+	</Navbar>
+);
